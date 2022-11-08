e999d651e35ea55f98c3b2ffea1e9b9d71c36467
fabmanri <fabmanri@publicisgroupe.net>
2022-11-08 11:02:14 -0500

    fix: build

--- components/collague.js
+++ components/collague.js
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
 import { ReactPhotoCollage } from "react-photo-collage";
 
-const Collague = ({ setting }) => {
-  useEffect(() => {
-    setTimeout(() => {
-      console.log("Delayed for 1 second.");
-      const el = document.querySelectorAll("[data-id]");
-      if (el) {
+export default function Collague ({ setting }) {
+useEffect(() => {
+  setTimeout(() => {
+    const el = document.querySelectorAll("[data-id]");
+      if (el) { 
         for (let index = 0; index < el.length; index++) {
-          //el[index].appendChild(null);
           let html = `<div class="abosulut-card"><h3>Felicidades ${setting.name[index]}!!!<br/>Has hecho un mergue ruquest exitosamente</h3> </div>`;
           let div = document.createElement("div");
           div.classList.add("hover-container");
@@ -16,9 +14,8 @@ const Collague = ({ setting }) => {
           el[index].appendChild(div);
         }
       }
-    }, "1000");
-  });
+  }, "1000");
+});
 
   return <ReactPhotoCollage {...setting} />;
 };
-export default Collague;
